c3f602b11a4077edf2c58168bf094da93e95a56e
PC-BAOTRUONG\bao.truong <truongvanbao126@gmail.com>
2025-05-07 16:09:05 +0700

    add ex 1

---
+++ myExercise_1/myExercise_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace myExercise_1
+{
+    internal class Program
+    {
+
+        public static async Task SendMessage(string yourMessage, string yourName, int time)
+        {
+            Console.WriteLine($"Welcome to chat room:  {yourName}");
+            await Task.Delay(time);
+            Console.WriteLine($"{yourName}: {yourMessage}");
+        }
+
+        static async Task Main(string[] args)
+        {
+
+             await SendMessage("how are you today?", "BaoBao", 1000);
+             await SendMessage("I am fine", "Truong", 900);
+
+
+            //SendMessage("how are you today?", "BaoBao", 1000);
+            //SendMessage("I am fine", "Truong", 1000);
+            Console.ReadKey();
+        }
+    }
+}
